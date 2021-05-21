e1fc9f0427f9fd6f5e2d3a19b13b8edc888346c9
Latte <chinolatte@icloud.com>
2021-05-21 14:31:54 +0900

    DBClassなかったから追加

---
+++ GroupC_theater/src/common/DBClass.java
@@ -0,0 +1,90 @@
+package common;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class DBClass {
+
+	// DB接続用コネクション
+	Connection objCon;
+
+	// 接続文字列
+	String connUrl = "";
+
+	public DBClass() {
+		try {
+		    Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+		    // フェッチ後はそれぞれの名前に変更
+		    // コミット前に空にしておくこと
+		    String serverName = "";	// サーバ名
+		    String dbName = "GroupC";						// データベース名
+
+		    String userName = "sa";							// ユーザ名
+		    String password = "Step2154822";				// パスワード
+
+		    connUrl = "jdbc:sqlserver://" + serverName + ";";
+		    connUrl += "database=" + dbName + ";";
+		    connUrl += "integratedSecurity=false;";
+		    connUrl += "user=" + userName + ";";
+		    connUrl += "password=" + password + ";";
+
+		}
+		catch(Exception objEx) {
+			System.err.println(objEx.getClass().getName() + ":" + objEx.getMessage());
+		}
+	}
+
+	/**
+	 * DB接続処理
+	 *
+	 * @return 接続結果（true:接続成功、false：接続失敗）
+	 */
+	public boolean dbOpen() {
+		try {
+
+		    // 接続開始
+			objCon =DriverManager.getConnection(connUrl);
+
+			System.out.println("DB接続成功");
+
+		    // 接続成功
+		    return true;
+		}
+		catch(Exception objEx) {
+			// エラー表示
+			System.err.println(objEx.getClass().getName() + ":" + objEx.getMessage());
+
+			// 接続失敗
+			return false;
+		}
+	}
+
+	/**
+	 * DB切断処理
+	 *
+	 * @return 切断結果（true:切断成功、false：切断失敗）
+	 */
+	public boolean dbClose() {
+		try {
+
+		    // 切断開始
+			objCon.close();
+
+			System.out.println("DB切断成功");
+
+		    // 切断成功
+		    return true;
+		}
+		catch(Exception objEx) {
+			// エラー表示
+			System.err.println(objEx.getClass().getName() + ":" + objEx.getMessage());
+
+			// 切断失敗
+			return false;
+		}
+	}
+
+}
+
+
